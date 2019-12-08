c50c8c051bc2d370d573dffee6f1fbb293bbe9e7
Napermial <h>
2019-12-08 10:24:54 +0100

    board added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import '../board/Board';
+import Board from "../board/Board";
 
 function App() {
   return (
     <div className="App">
+       <Board/>
     </div>
   );
 }
---
+++ src/components/board/Board.js
@@ -0,0 +1,15 @@
+import './Board.css';
+import 'react-dom';
+import React from "react";
+
+function Board() {
+    return (
+        <div className="Board">
+            <svg height={600} width={600}>
+
+            </svg>
+        </div>
+    );
+}
+
+export default Board;
